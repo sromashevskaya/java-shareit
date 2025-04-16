beab17dd25731d45de59c053f9ffe2f8b034a1e7
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2025-04-16 19:12:14 +0300

    Исправлены замечания ревьюера

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,15 +1,27 @@
 package ru.practicum.shareit.item;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
-@Mapper(componentModel = "spring")
-public interface ItemMapper {
-    ItemMapper INSTANCE = Mappers.getMapper(ItemMapper.class);
+@Component
+public class ItemMapper {
+    public ItemDto toItemDto(Item item) {
+        return new ItemDto(item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable(),
+                item.getOwner(),
+                item.getRequest() != null ? item.getRequest() : null
+        );
+    }
 
-    ItemDto toItemDto(Item item);
-
-    Item toItem(ItemDto itemDto);
+    public Item toItem(ItemDto itemDto) {
+        return new Item(itemDto.getId(),
+                itemDto.getName(),
+                itemDto.getDescription(),
+                itemDto.getAvailable(),
+                itemDto.getOwner(),
+                itemDto.getRequest() != null ? itemDto.getRequest() : null);
+    }
 }
