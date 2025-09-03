e4b560bef8c5289d365a5ae9aca4a1df5e43a6a1
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2025-09-03 17:45:02 +0300

    Исправление ошибок

--- server/src/main/java/ru/practicum/shareit/booking/BookingService.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
-
 import java.util.List;
 
 public interface BookingService {
